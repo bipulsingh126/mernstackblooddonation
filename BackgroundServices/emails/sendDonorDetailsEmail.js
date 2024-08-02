f2ea3f139addeeb4db33bb41ab558c5a2382869b
bipulsingh126 <bipulpratqpsingh126@gmail.com>
2024-08-02 23:08:28 +0530

    finsh

--- BackgroundServices/emails/sendDonorDetailsEmail.js
+++ BackgroundServices/emails/sendDonorDetailsEmail.js
@@ -1,29 +1,54 @@
 import ejs from 'ejs';
 import dotenv from 'dotenv';
 import { Donor } from '../models/Donor.js';
+import { sendMail } from '../helpers/sandmail.js';
 
 
 dotenv.config();
 
 
-const sendDonorDetailsEmail = async ()=>{
+const sendDonorDetailsEmail = async () => {
 
-    const donors = await Donor.find({status : 0});
+    const donors = await Donor.find({ status: 0 });
 
     if (donors.length > 0) {
-        for(let donor of donors){
+        for (let donor of donors) {
             ejs.renderFile(
                 "templates/BloodDonationDonor.ejs",
                 {
                     name: donor.name,
-                    email : donor.email,
-                    tel : donor.tel,
-                    address : donor.address,
+                    email: donor.email,
+                    tel: donor.tel,
+                    address: donor.address,
+                    bloodgroup: donor.bloodgroup,
+                    diseases: donor.diseases,
+                    weight: donor.weight,
+                    bloodpressure: donor.bloodpressure,
+                    age: donor.age,
+                    date: donor.date
+                },
+                async (err, data) => {
+                    let messageoption = {
+                        from: process.env.EMAIL,
+                        to: donor.email,
+                        subject: "Hello, Bridge Donor. ",
+                        html: data
+                    };
+                    try {
+                        await sendMail(messageoption);
+                        await Donor.findByIdAndUpdate(donor._id,
+                            {
+                                $set: { status: 1 }
+                            }
+                        );
+                    } catch (error) {
+                        console.log(error);
+                    }
                 }
-            )
+            );
         }
     }
 };
 
 
-export {sendDonorDetailsEmail};
+export { sendDonorDetailsEmail };
