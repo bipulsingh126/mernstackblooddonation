1aecb263b0768bca8902902c8f6faaa46a863d7f
bipulsingh126 <bipulpratqpsingh126@gmail.com>
2024-08-04 03:35:46 +0530

    footer-section

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,16 +1,27 @@
+import Contact from "../Components/Contact.jsx";
 import Featured from "../Components/Featured.jsx";
+import Footer from "../Components/Footer.jsx";
 import Hero from "../Components/Hero.jsx";
 import Navbar from "../Components/Navbar.jsx";
-
+import { Element } from "react-scroll";
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <Featured/>
-    </div>
-  )
-}
+      <Navbar />
+      <Element name="hero">
+        <Hero />
+      </Element>
+      <Element name="featured">
+        <Featured />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
 
-export default Home
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
