c069f5e985c1d1952d4daf0dab889434fbd63ea3
bipulsingh126 <bipulpratqpsingh126@gmail.com>
2024-08-09 02:57:23 +0530

    finsh

--- frontend/src/Pages/Donor.jsx
+++ frontend/src/Pages/Donor.jsx
@@ -1,83 +1,104 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { publicRequest } from "../../requestMethods.js";
 
 const Donor = () => {
-  
+  const [donor, setDonor] = useState({});
+
+  const location = useLocation();
+
+  const donorId = location.pathname.split("/")[3];
+
+  useEffect(() => {
+    const getDonor = async () => {
+      try {
+        const res = await publicRequest.get(`/donors/find/${donorId}`);
+        setDonor(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getDonor();
+  }, []);
+
   return (
     <div className="flex items-center justify-center min-h-screen ">
-    <div className="m-[20px] p-[20px] h-[80vh] w-[450px] shadow-lg ">
-      <h2 className="font-semibold ">Donor</h2>
-      <div className="flex flex-col my-[12px] ">
-        <label htmlFor="">Name</label>
-        <input
-          type="text"
-          placeholder="Khusi Singh"
-          className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
-        />
-      </div>
-      <div className="flex flex-col my-[12px] ">
-        <label htmlFor="">Address</label>
-        <input
-          type="text"
-          placeholder="Kanpur"
-          className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
-        />
-      </div>
-      <div className="flex flex-col my-[12px] ">
-        <label htmlFor="">Phone</label>
-        <input
-          type="text"
-          placeholder="01717171717"
-          className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
-        />
-      </div>
-      <div className="flex flex-col my-[12px] ">
-        <label htmlFor="">BloodType</label>
-        <input
-          type="text"
-          placeholder="AB"
-          className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
-        />
+      <div className="m-[20px] p-[20px] h-[80vh] w-[450px] shadow-lg ">
+        <h2 className="font-semibold ">Donor</h2>
+        <div className="flex flex-col my-[12px] ">
+          <label htmlFor="">Name</label>
+          <input
+            type="text"
+            placeholder={donor.name}
+            className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
+          />
+        </div>
+        <div className="flex flex-col my-[12px] ">
+          <label htmlFor="">Address</label>
+          <input
+            type="text"
+            placeholder={donor.address}
+            className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
+          />
+        </div>
+        <div className="flex flex-col my-[12px] ">
+          <label htmlFor="">Tel</label>
+          <input
+            type="text"
+            placeholder={donor.tel}
+            className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
+          />
+        </div>
+        <div className="flex flex-col my-[12px] ">
+          <label htmlFor="">Bloodgroup</label>
+          <input
+            type="text"
+            placeholder={donor.bloodgroup}
+            className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
+          />
+        </div>
+
+        <div className="flex flex-col my-[12px] ">
+          <label htmlFor="">email</label>
+          <input
+            type="eamil"
+            placeholder={donor.email}
+            className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
+          />
+        </div>
       </div>
 
-      <div className="flex flex-col my-[12px] ">
-        <label htmlFor="">email</label>
-        <input
-          type="text"
-          placeholder="katixo3505@leacore.com"
-          className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
-        />
+      <div className="m-[20px] p-[20px] h-[80vh] w-[450px] shadow-lg ">
+        <div className="flex flex-col my-[12px] ">
+          <label htmlFor="">Weghit</label>
+          <input
+            type="200 pounds"
+            placeholder={`${donor.weghit} kg`}
+            className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
+          />
+        </div>
+        <div className="flex flex-col my-[12px] ">
+          <label htmlFor="">Date</label>
+          <input
+            type="date"
+            placeholder={donor.date}
+            className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
+          />
+        </div>
+        <div className="flex flex-col my-[12px] ">
+          <label htmlFor="">Diseases</label>
+          <input
+            type="text"
+            placeholder={donor.diseases}
+            className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
+          />
+        </div>
+        <button className="bg-[#444] cursor-pointer text-white p-[10px] w-[300px]  ">
+          Update
+        </button>
       </div>
     </div>
-
-    <div className="m-[20px] p-[20px] h-[80vh] w-[450px] shadow-lg ">
-    <div className="flex flex-col my-[12px] ">
-        <label htmlFor="">Weghit</label>
-        <input
-          type="200 pounds"
-          placeholder="katixo3505@leacore.com"
-          className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
-        />
-      </div>
-      <div className="flex flex-col my-[12px] ">
-        <label htmlFor="">Date</label>
-        <input
-          type="date"
-          placeholder="katixo3505@leacore.com"
-          className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
-        />
-      </div>
-      <div className="flex flex-col my-[12px] ">
-        <label htmlFor="">Diseases</label>
-        <input
-          type="text"
-          placeholder="Hypertension"
-          className="border-b-2 border-b-[#555] border-solid outline-none p-[10px] w-[300px] "
-        />
-      </div>
-       <button className="bg-[#444] cursor-pointer text-white p-[10px] w-[300px]  ">Update</button>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
 export default Donor;
--- frontend/src/Pages/Prospect.jsx
+++ frontend/src/Pages/Prospect.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { publicRequest } from "../../requestMethods.js";
+
 const Prospect = () => {
+  const [prospect, setProspect] = useState({});
+
+  const location = useLocation();
+  const prospectId = location.pathname.split("/")[3];
+
+  useEffect(() => {
+    const getProspect = async () => {
+      try {
+        const res = await publicRequest.get(`/prospects/find/${prospectId}`);
+        setProspect(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProspect();
+  }, []);
+
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <div className="m-[20px] h-[80vh] w-[450px] shadow-lg ">
@@ -6,28 +27,28 @@ const Prospect = () => {
 
         <ul className="m-[30px]">
           <li className=" mt-[10px] ">
-            <strong className="font-semibold">Name:</strong>Khusi Singh
+            <strong className="font-semibold">Name:</strong> {prospect.name}
           </li>
           <li className=" mt-[10px] ">
-            <strong className="font-semibold">Address:</strong>Kanpur
+            <strong className="font-semibold">Address:</strong> {prospect.address}
           </li>
           <li className=" mt-[10px] ">
-            <strong className="font-semibold">Phone:</strong>01717171717
+            <strong className="font-semibold">Phone:</strong>{prospect.phone}
           </li>
           <li className=" mt-[10px] ">
-            <strong className="font-semibold">BloodType:</strong>AB
+            <strong className="font-semibold">BloodType:</strong>{prospect.bloodgroup}
           </li>
           <li className=" mt-[10px] ">
-            <strong className="font-semibold">age:</strong>23
+            <strong className="font-semibold">age:</strong>{prospect.age}
           </li>
           <li className=" mt-[10px] ">
-            <strong className="font-semibold">gender:</strong>female
+            <strong className="font-semibold">gender:</strong>{prospect.gender}
           </li>
           <li className=" mt-[10px] ">
-            <strong className="font-semibold">Weight:</strong>60kg
+            <strong className="font-semibold">Weight:</strong>{prospect.weight}
           </li>
           <li className=" mt-[10px] ">
-            <strong className="font-semibold">Status:</strong>pending
+            <strong className="font-semibold">Status:</strong>{prospect.status}
           </li>
         </ul>
         <span className="block m-[10px] ">
